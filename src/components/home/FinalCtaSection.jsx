80781567f67775cb0abfbf50689ad05b2ee90721
khizaramin403 <khizaramin403@gmail.com>
2026-09-16 14:08:29 -0700

    contact page changes

--- src/components/home/FinalCtaSection.jsx
+++ src/components/home/FinalCtaSection.jsx
@@ -218,9 +218,9 @@ const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw9Y8GQyClTl_
                     <MapPin className="w-3.5 h-3.5 text-red-400" />
                   </div>
                   <div className="min-w-0 flex-1">
-                    <span className="block text-[9px] text-neutral-500 uppercase tracking-wider">Service Hub</span>
+                    <span className="block text-[9px] text-neutral-500 uppercase tracking-wider">Morris, MN & surrounding areas </span>
                     <span className="block text-xs font-semibold text-white">
-                      Morris, MN 56267
+                      Mobile Service Area
                     </span>
                   </div>
                 </div>
